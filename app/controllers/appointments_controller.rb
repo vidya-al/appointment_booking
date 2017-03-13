331b11e1226c61f975f70ce6c15ebf4b2c699574
vidya-al <vidya.al@tcs.com>
2017-03-13 16:37:41 +0530

    initial commit

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,71 @@
+class AppointmentsController < ApplicationController
+  
+  # GET /appointments
+  # GET /appointments.json
+  def index
+    @appointments = Appointment.all
+  end
+
+  # GET /appointments/1
+  # GET /appointments/1.json
+  def show
+  end
+
+  # GET /appointments/new
+  def new
+    @appointment = Appointment.new
+    @appointment_date = params[:selected_date].present? ? params[:selected_date] : Time.now.strftime("%d/%m/%Y")
+    @date = DateTime.parse(@appointment_date).strftime("%Y-%m-%d")
+    @appointments = Appointment.where("appointment_day = '#{@date}'")
+  end
+
+  # GET /appointments/1/edit
+  def edit
+  end
+
+  # POST /appointments
+  # POST /appointments.json
+  def create
+    @appointment = Appointment.new(appointment_params)
+      if @appointment.save
+        redirect_to new_appointment_path(:selected_date => @appointment.appointment_day.strftime("%d/%m/%Y")), :notice=> 'Your Appointment is confirmed.'
+       else
+        render :new, :notice=> @appointment.errors
+      end   
+  end
+
+  # PATCH/PUT /appointments/1
+  # PATCH/PUT /appointments/1.json
+  def update
+    respond_to do |format|
+      if @appointment.update(appointment_params)
+        format.html { redirect_to @appointment, notice: 'Appointment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @appointment }
+      else
+        format.html { render :edit }
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /appointments/1
+  # DELETE /appointments/1.json
+  def destroy
+    @appointment.destroy
+    respond_to do |format|
+      format.html { redirect_to appointments_url, notice: 'Appointment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_appointment
+      @appointment = Appointment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def appointment_params
+      params.require(:appointment).permit(:appointment_day, :time_slot, :confirm_booking)
+    end
+end
---
+++ app/models/appointment.rb
@@ -0,0 +1,6 @@
+class Appointment < ApplicationRecord
+  
+  Time_Slots = ["10:00 AM","11:00 AM","12:00 PM","1:00 PM","3:00 PM","4:00 PM","5:00 PM","6:00 PM"]
+
+end
+
